66bfd3b79c6152ec08d4ab9b5b3a164d2aef3fcd
Suzana Suvajac <susanes94@gmail.com>
2026-03-17 15:21:53 +0800

    init project setup (init commit)

---
+++ backend/src/main/java/com/maplewood/CoursePlanningApplication.java
@@ -0,0 +1,11 @@
+package com.maplewood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlanningApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CoursePlanningApplication.class, args);
+  }
+}
